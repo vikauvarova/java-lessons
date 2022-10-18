4305598698974333ee511aa2a68f4ee1dd0a6295
vikauvarova <vg220915@gmail.com>
2022-10-18 11:38:06 +0300

    add homework8

---
+++ src/homework8/FirstLevel.java
@@ -0,0 +1,71 @@
+package homework8;
+
+import java.util.Scanner;
+import java.util.SortedMap;
+
+public class FirstLevel {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите с какого года начать проверку");
+        int yearFrom = scanner.nextInt();
+        System.out.println("Введите до какого года проверять");
+        int yearTo = scanner.nextInt();
+        int count = 0;
+        for (int i = yearFrom; i < yearTo; i++) {
+           if (i % 4 == 0){
+               count++;
+           };
+        }
+        System.out.println(count + " високосных лет");
+
+        int num = 43;
+        int amountOfNumbers = 0;
+        for (int i = 1; i < 10; i++) {
+              if (num % i == 0) {
+                  amountOfNumbers++;
+              }
+        }
+        if (amountOfNumbers == 1){
+            System.out.println(num + " - простое число");
+        } else {
+            System.out.println(num + " - не простое число");
+        }
+
+        for (int i = 10; i < 100; i++) {
+            if (i % 2 == 0 && i % 6 != 0) {
+                System.out.println(i);
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i <= 100; i++) {
+            sum = i + sum;
+        }
+        System.out.println(sum);
+
+        for (int i = -10; i > -41; i--) {
+            System.out.println(i);
+        }
+
+        long result = 1;
+        for (int i = 1; i < 100; i++) {
+            if (i % 13 == 0){
+                result = i * result;
+            }
+        }
+        System.out.println(result);
+
+        System.out.println("Введите множитель");
+        int factorOne = scanner.nextInt();
+        System.out.println("Введите сомножитель");
+        int factorTwo = scanner.nextByte();
+        System.out.println("Результат умножения первого числа на второе ");
+        int product = scanner.nextInt();
+        if (factorOne * factorTwo == product){
+            System.out.println("Это правильно");
+        } else {
+            System.out.println("Это не правильно");
+            System.out.println("Правильный ответ " + factorOne * factorTwo);
+        }
+    }
+}
---
+++ src/homework8/SecondLevel.java
@@ -0,0 +1,31 @@
+package homework8;
+
+public class SecondLevel {
+    public static void main(String[] args) {
+        int factorOne = 6;
+        int factorTwo = 2;
+        int product = 0;
+        for (int i = 0; i < factorTwo ; i++) {
+            product = product + factorOne;
+        }
+        System.out.println(product);
+
+        int result = 0;
+        int count = 0;
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 != 0){
+                result = i + result;
+                count++;
+            }
+        }
+        System.out.println(result/count);
+
+        int n = 3;
+        int factorial = 1;
+        for (int i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
+        System.out.println(factorial);
+
+    }
+}
